f81a63480048faee4b1a23f2364f61b94ef0ecd2
Ethan Bowering <ethanbowering24@gmail.com>
2026-02-09 17:18:28 +0000

    not quite working

--- PiClaw_Client/include/mpu.h
+++ PiClaw_Client/include/mpu.h
@@ -1,44 +1,41 @@
-#include "MPU6050_6Axis_MotionApps20.h"
-#include "wiringPi.h"
 #include "MPU6050.h"
+#include "Fusion.h"
+
+#define SAMPLE_RATE 10
 
 class MPU
 {
 private:
     MPU6050 mpu6050;
-
-    //int fifoAlive = 0; // tests if the interrupt is triggering
-    //int isAlive = -20;     // counts interrupt start at -20 to get 20+ good values before assuming connected
-    // MPU control/status vars
-    //uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
-    uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
-    uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
-    //uint16_t fifoCount;     // count of all bytes currently in FIFO
-    uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-    // orientation/motion vars
-    Quaternion q;           // [w, x, y, z]         quaternion container
-    //VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-    //VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-    //VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-    VectorFloat gravity;    // [x, y, z]            gravity vector
-    float euler[3];         // [psi, theta, phi]    Euler angle container
-    float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-    //uint8_t startUP = 100; // lets get 100 readings from the MPU before we start trusting them (Bot is not trying to balance at this point it is just starting up.)
-
-    //volatile bool interrupt = false;     // indicates whether MPU interrupt pin has gone high
-    
     int MPUOffsets[6] = {-262, 1702, 1307 , 193, 103, 74};// vert board
-    //int interruptPin = 26;
+
+    const FusionMatrix gyroscopeMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+    const FusionVector gyroscopeSensitivity = {1.0f, 1.0f, 1.0f};
+    const FusionVector gyroscopeOffset = {0.0f, 0.0f, 0.0f};
+
+    const FusionMatrix accelerometerMisalignment = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+    const FusionVector accelerometerSensitivity = {1.0f, 1.0f, 1.0f};
+    const FusionVector accelerometerOffset = {0.0f, 0.0f, 0.0f};
+
+    const FusionMatrix softIronMatrix = {1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f};
+    const FusionVector hardIronOffset = {0.0f, 0.0f, 0.0f};
+
+    const FusionAhrsSettings settings = {
+        .convention = FusionConventionNwu,
+        .gain = 0.5f,
+        .gyroscopeRange = 2000.0f, /* replace with actual gyroscope range */
+        .accelerationRejection = 10.0f,
+        .magneticRejection = 10.0f,
+        .recoveryTriggerPeriod = 5 * SAMPLE_RATE, /* 5 seconds */
+    };
+
+    FusionBias bias;
+    FusionAhrs ahrs;
 
 public:
     MPU();
     MPU(uint8_t mpu_address);
     bool Connect();
-    //void GetDMP();
-    //void MPUMath();
-    //void DmpDataReady();
-    void Loop();
+    void ReadFusion();
 
-    //static void ISR(struct WPIWfiStatus wfiStatus, void* userdata);
 };
--- PiClaw_Client/src/mpu.cpp
+++ PiClaw_Client/src/mpu.cpp
@@ -1,10 +1,9 @@
 #include "mpu.h"
 #include "I2Cdev.h"
-#include "wiringPi.h"
+#include "Fusion.h"
+#include "time.h" //move to chrono
+
 #include <iostream>
-#include <bitset>
-#include <algorithm>
-#include <iterator>
 
 MPU::MPU():mpu6050()
 {
@@ -28,179 +27,70 @@ bool MPU::Connect()
         std::cout << "MPU6050 Connection Failed" << std::endl;
         return false;
     }
-    
 
-    for (int ctr = 1; ctr <= 10; ctr++)
-    {
-        mpu6050.initialize();
-        devStatus = mpu6050.dmpInitialize();
-        if (devStatus == 0)
-        {
-            break;
-        }
-        // ERROR!
-        // 1 = initial memory load failed
-        // 2 = DMP configuration updates failed
-        // (if it's going to break, usually the code will be 1)
-        char * StatStr[5] { "No Error", "initial memory load failed", "DMP configuration updates failed", "3", "4"};
-        std::cout << "MPU init Try #" << ctr << "\nDMP Init failed (code " << StatStr[devStatus] << ")" << std::endl;
+    mpu6050.initialize(); 
 
-        if (ctr == 10)
-        {
-            std::cout << "Max MPU connection attempts reached" << std::endl;
-            return false;
-        }
-        
-        delay(1000);
-    }   
-
-    mpu6050.setXAccelOffset(MPUOffsets[0]);
-    mpu6050.setYAccelOffset(MPUOffsets[1]);
-    mpu6050.setZAccelOffset(MPUOffsets[2]);
-    mpu6050.setXGyroOffset(MPUOffsets[3]);
-    mpu6050.setYGyroOffset(MPUOffsets[4]);
-    mpu6050.setZGyroOffset(MPUOffsets[5]);
-
-    std::cout << "Enabling DMP..." << std::endl;
-    mpu6050.setDMPEnabled(true);
-
-    std::cout << "Enabling interrupt detection" << std::endl;
-    mpu6050.setInterruptDrive(0);
-    std::cout << "mpu.getInterruptDrive " << mpu6050.getInterruptDrive() << std::endl;
-
-    std::cout << std::bitset<8>(mpu6050.getIntEnabled()) << std::endl;
-    //wiringPiISR2(interruptPin, INT_EDGE_RISING, &ISR, 3000, this);
-    //mpuIntStatus = mpu6050.getIntStatus();
-
-    // get expected DMP packet size for later comparison
-    packetSize = mpu6050.dmpGetFIFOPacketSize();
-    //delay(1000); // Let it Stabalize
-
-    //mpu6050.resetFIFO(); // Clear fifo buffer
-    //mpu6050.getIntStatus();
-    //interrupt = false; // wait for next interrupt
+    FusionBiasInitialise(&bias, SAMPLE_RATE);
+    FusionAhrsInitialise(&ahrs);
+    FusionAhrsSetSettings(&ahrs, &settings);
     return true;
 }
-/*
-void MPU::GetDMP() { // Best version I have made so far
-  // Serial.println(F("FIFO interrupt at:"));
-  // Serial.println(micros());
-  static unsigned long LastGoodPacketTime;
-  interrupt = false;
-  fifoAlive = 1;
-  fifoCount = mpu6050.getFIFOCount();
-  if ((!fifoCount) || (fifoCount % packetSize)) { // we have failed Reset and wait till next time!
-    mpu6050.resetFIFO();// clear the buffer and start over
-  } else {
-    while (fifoCount  >= packetSize) { // Get the packets until we have the latest!
-      mpu6050.getFIFOBytes(fifoBuffer, packetSize); // lets do the magic and get the data
-      fifoCount -= packetSize;
-    }
-    LastGoodPacketTime = millis();
-    MPUMath(); // <<<<<<<<<<<<<<<<<<<<<<<<<<<< On success MPUMath() <<<<<<<<<<<<<<<<<<<
-  }
-}
 
-void MPU::DmpDataReady(void)
+void MPU::ReadFusion()
 {
-    interrupt=true;
-}
+    struct timespec remaining, request = { 0, 1000000000L / SAMPLE_RATE };
+    clock_t previousTimestamp;
 
-void MPU::MPUMath()
-{
-    mpu6050.dmpGetQuaternion(&q, fifoBuffer);
-    mpu6050.dmpGetGravity(&gravity, &q);
-    //mpu6050.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    //float yaw = (ypr[0] * 180.0 / M_PI);
-    //float pitch = (ypr[1] *  180.0 / M_PI);
-    //float roll = (ypr[2] *  180.0 / M_PI);
+    while (true)
+    {
+        const clock_t timestamp = clock();
+        int16_t ax, ay, az;
+        int16_t gx, gy, gz;
+        mpu6050.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
     
-    delay(100);
-    Serial.print(q.w);Serial.print(",");
-    Serial.print(q.x);Serial.print(",");
-    Serial.print(q.y);Serial.print(",");
-    Serial.print(q.z);
-    Serial.print("\n");
+        FusionVector gyroscope = {gx, gy, gz};
+        FusionVector accelerometer = {ax, ay, az};
+        //FusionVector magnetometer = {1.0f, 0.0f, 0.0f};
+    
+        gyroscope = FusionModelInertial(gyroscope, gyroscopeMisalignment, gyroscopeSensitivity, gyroscopeOffset);
+        accelerometer = FusionModelInertial(accelerometer, accelerometerMisalignment, accelerometerSensitivity, accelerometerOffset);
+        //magnetometer = FusionModelMagnetic(magnetometer, softIronMatrix, hardIronOffset);
+    
+        gyroscope = FusionBiasUpdate(&bias, gyroscope);
+    
+        // Calculate delta time to compensate for gyroscope sample clock errors
+        const float deltaTime = (float) (timestamp - previousTimestamp) / (float) CLOCKS_PER_SEC;
+        previousTimestamp = timestamp;
 
-    std::cout << q.w << ", " << q.x << ", " << q.y << ", " << q.z << std::endl;
-};
+        //FusionAhrsUpdate(&ahrs, gyroscope, accelerometer, magnetometer, deltaTime);
+        FusionAhrsUpdateNoMagnetometer(&ahrs, gyroscope, accelerometer, deltaTime);
 
-void MPU::ISR(struct WPIWfiStatus wfiStatus, void* userdata)
-{
-    auto* mpu = static_cast<MPU*>(userdata);
-    //mpu->GetDMP();
-    mpu->interrupt = true;
-};
-*/
-void MPU::Loop()
-{
-    std::cout << "Discarding 100 readings..." << std::endl;
-    for (int i = 0; i < 100; i++)   
-    {
-        mpu6050.dmpGetCurrentFIFOPacket(fifoBuffer);
-        delay(500);
-    } 
+        // Print AHRS outputs
+        const FusionEuler euler = FusionQuaternionToEuler(FusionAhrsGetQuaternion(&ahrs));
 
-    const int samples = 20000;
-    float yaw[samples];
-    float pitch[samples];
-    float roll[samples];
+        const FusionVector earth = FusionAhrsGetEarthAcceleration(&ahrs);
 
-    for (int i = 0; i < samples; i++)   
-    {
-        std::cout << "\nLOOP " << i << ":" << std::endl;
-        if (mpu6050.dmpGetCurrentFIFOPacket(fifoBuffer))
-        {
-            mpu6050.dmpGetQuaternion(&q, fifoBuffer);
-            std::cout << "Quaternion: " << q.w << ", " << q.x << ", " << q.y << ", " << q.z << std::endl;
+        printf("Roll %0.1f, Pitch %0.1f, Yaw %0.1f, X %0.1f, Y %0.1f, Z %0.1f\n",
+               euler.angle.roll, euler.angle.pitch, euler.angle.yaw,
+               earth.axis.x, earth.axis.y, earth.axis.z);
 
-            mpu6050.dmpGetGravity(&gravity, &q);
-            std::cout << "Gravity :" << gravity.x << ", " << gravity.y << ", " << gravity.z << std::endl;
-
-            mpu6050.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            yaw[i] = (ypr[0] * 180.0 / M_PI);
-            pitch[i] = (ypr[1] *  180.0 / M_PI);
-            roll[i] = (ypr[2] *  180.0 / M_PI);
-            std::cout << "YPR: " << yaw[i] << ", " << pitch[i] << ", " << roll[i] << std::endl;  
-        }
-        else
-        {
-            std::cout << "READ " << i << "FAILED" << std::endl;
-        }
-        delay(500);
-    } 
-
-    std::cout << std::endl;
-
-    std::cout << "YAW START: " << yaw[0] << ", YAW END: " << yaw[samples-1] << std::endl;
-    auto yawMin = *std::min_element(std::begin(yaw), std::end(yaw));
-    auto yawMax = *std::max_element(std::begin(yaw), std::end(yaw));
-    std::cout << "YAW MIN: " << yawMin << ", YAW MAX: " << yawMax << ", RANGE: " << abs(yawMax-yawMin) << "\n" << std::endl;
-
-    std::cout << "PITCH START: " << pitch[0] << ", PITCH END: " << pitch[samples-1] << std::endl;
-    auto pitchMin = *std::min_element(std::begin(pitch), std::end(pitch));
-    auto pitchMax = *std::max_element(std::begin(pitch), std::end(pitch));
-    std::cout << "PITCH MIN: " << pitchMin << ", PITCH MAX: " << pitchMax << ", RANGE: " << abs(pitchMax-pitchMin) << "\n" << std::endl;
-
-    std::cout << "ROLL START: " << roll[0] << ", ROLL END: " << roll[samples-1] << std::endl;
-    auto rollMin = *std::min_element(std::begin(roll), std::end(roll));
-    auto rollMax = *std::max_element(std::begin(roll), std::end(roll));
-    std::cout << "ROLL MIN: " << rollMin << ", ROLL MAX: " << rollMax << ", RANGE: " << abs(rollMax-rollMin) << "\n" << std::endl;
+        nanosleep(&request, &remaining);
+        
+    }
 
 }
 
 int main()
 {
 
-    wiringPiSetupPinType(WPI_PIN_PHYS);
-    pinMode(26, INPUT);
+    //wiringPiSetupPinType(WPI_PIN_PHYS);
+    //pinMode(26, INPUT);
     I2Cdev::initialize("/dev/i2c-1");
 
     MPU mpu;
 
     mpu.Connect();
-    delay(1000);
-    mpu.Loop();
+    mpu.ReadFusion();
 
 }
 
