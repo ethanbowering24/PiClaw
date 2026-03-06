26e01bf0fc500bc22a37190b1d3a819fcf3381d1
Ethan Bowering <ethanbowering24@gmail.com>
2026-03-06 03:04:17 +0000

    Working, fixed yaw problem. Removed unneeded DMP files

--- PiClaw_Client/include/mpu.h
+++ PiClaw_Client/include/mpu.h
@@ -9,16 +9,18 @@ class MPU
 private:
     I2Cdev i2cDev;
     MPU6050 mpu6050;
-    int MPUOffsets[6] = {-262, 1702, 1307 , 193, 103, 74};// vert board
+    //int MPUOffsets[6] = {-262, 1702, 1307 , 193, 103, 74};// vert board
 
+    /*
     const FusionAhrsSettings settings = {
         .convention = FusionConventionNwu,
         .gain = 0.5f,
-        .gyroscopeRange = 250.0f, /* replace with actual gyroscope range */
+        .gyroscopeRange = 0,
         .accelerationRejection = 0,
         .magneticRejection = 0,
         .recoveryTriggerPeriod = 0
     };
+    */
 
     FusionBias bias;
     FusionAhrs ahrs;
--- PiClaw_Client/src/mpu.cpp
+++ PiClaw_Client/src/mpu.cpp
@@ -22,8 +22,8 @@ MPU::MPU(const char* devPath, uint8_t mpu_address) : i2cDev(devPath), mpu6050(i2
 
 void MPU::Calibrate()
 {
-    mpu6050.CalibrateAccel(255);
-    mpu6050.CalibrateGyro(255);
+    mpu6050.CalibrateAccel();
+    mpu6050.CalibrateGyro();
 
     mpu6050.PrintActiveOffsets();
     // save to a file
@@ -43,26 +43,20 @@ bool MPU::Connect()
         return false;
     }
     
-
     mpu6050.initialize(); 
-    std::cout <<"init"<< std::endl;
-    
-    mpu6050.setDLPFMode(0x03); // set DLPF
+    mpu6050.setDLPFMode(0x03);          // set DLPF
     mpu6050.setExternalFrameSync(0x00); // disable FSYNC
-
-    //Sample Rate = Gyroscope Output Rate / (1 + SMPLRT_DIV)
-    //  1000/(1+4) = 200Hz
-    mpu6050.setRate(0x04);
-
-    //load calibration params
-
+    mpu6050.setRate(0x04);              // Sample Rate = Gyroscope Output Rate / (1 + SMPLRT_DIV)
+                                        //      1000/(1+4) = 200Hz
+    
+/*
     std::cout << "RATE: " << static_cast<int>(mpu6050.getRate()) << std::endl;
     std::cout << "FSYNC: " << static_cast<int>(mpu6050.getExternalFrameSync()) << std::endl;
     std::cout << "DLPF MODE: " << static_cast<int>(mpu6050.getDLPFMode()) << std::endl;
     std::cout << "CLK: " << static_cast<int>(mpu6050.getClockSource()) << std::endl;
     std::cout << "Gscale: " << static_cast<int>(mpu6050.getFullScaleGyroRange()) << std::endl;
     std::cout << "Ascale: " << static_cast<int>(mpu6050.getFullScaleAccelRange()) << std::endl;
-
+*/
     return true;
 }
 
@@ -71,7 +65,8 @@ void MPU::ReadFusion()
 
     FusionBiasInitialise(&bias, SAMPLE_RATE);
     FusionAhrsInitialise(&ahrs);
-    FusionAhrsSetSettings(&ahrs, &settings);
+    //FusionAhrsSetSettings(&ahrs, &settings);
+
     auto next_loop_time = std::chrono::steady_clock::now();
     auto previousTime = std::chrono::steady_clock::now();
 
@@ -82,10 +77,10 @@ void MPU::ReadFusion()
         int16_t gx, gy, gz;
         mpu6050.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
     
+        //TODO replace magic numbers with named values
         FusionVector gyroscope = {gx/131.0f, gy/131.0f, gz/131.0f};
         FusionVector accelerometer = {ax/16384.0f, ay/16384.0f, az/16384.0f};
         //FusionVector magnetometer = {1.0f, 0.0f, 0.0f};
-        
         gyroscope = FusionBiasUpdate(&bias, gyroscope);
     
         // Calculate delta time to compensate for gyroscope sample clock errors
@@ -106,7 +101,26 @@ void MPU::ReadFusion()
                euler.angle.roll, euler.angle.pitch, euler.angle.yaw,
                earth.axis.x, earth.axis.y, earth.axis.z);
 
-
+        /*
+        FusionAhrsFlags flags = FusionAhrsGetFlags(&ahrs);
+        if (flags.accelerationRecovery)
+        {
+            std::cout << "ACCELERATION RECOVERY" << std::endl;
+        }
+        if (flags.angularRateRecovery)
+        {
+            std::cout << "ANGULAR RECOVERY" << std::endl;
+        }
+        if (flags.initialising)
+        {
+            std::cout << "INITIALIZING" << std::endl;
+        }
+        if (flags.magneticRecovery)
+        {
+            std::cout << "MAGNETIC RECOVERY" << std::endl;
+        }   
+        */
+        
         // 5ms for 200Hz
         next_loop_time += std::chrono::milliseconds(5);
         std::this_thread::sleep_until(next_loop_time);
@@ -120,8 +134,6 @@ int main(int argc, char* argv[])
     MPU mpu("/dev/i2c-1");
 
     mpu.Connect();
-    
-
 
     //wiringPiSetupPinType(WPI_PIN_PHYS);
     //pinMode(26, INPUT);
