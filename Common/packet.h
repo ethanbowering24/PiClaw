55b2b7730e1f3debb9bf6146208408fc8fb34fb7
ethanbowering24 <ethanbowering24@gmail.com>
2026-03-31 11:16:16 -0400

    add packet to string function

--- Common/packet.h
+++ Common/packet.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#include <sstream>
+#include <string>
+#include <iomanip> // Required for std::fixed and std::setprecision
+
+
 enum {
-    WRIST_PITCH = 0,
-    WRIST_ROLL,
+    WRIST_ROLL = 0,
+    WRIST_PITCH,
     WRIST_YAW,
-    FOREARM_PITCH,
     FOREARM_ROLL,
+    FOREARM_PITCH,
     FOREARM_YAW,
-    UPARM_PITCH,
     UPARM_ROLL,
+    UPARM_PITCH,
     UPARM_YAW,
     INDEX_COUNT  // bonus: tracks array size
 };
@@ -16,4 +21,19 @@ enum {
 struct Packet {
     int id;
     float values[INDEX_COUNT];
+
+    std::string PacketToString()
+    {
+        std::ostringstream ss;
+        ss << std::fixed << std::setprecision(2);
+        ss << "Packet ID: " << id << "\n";
+        ss << "            Roll    Pitch    Yaw\n";
+        ss << "Wrist:      " << values[WRIST_ROLL]      << "    " << values[WRIST_PITCH]    << "    " << values[WRIST_YAW]      << "\n";
+        ss << "Forearm:    " << values[FOREARM_ROLL]    << "    " << values[FOREARM_PITCH]  << "    " << values[FOREARM_YAW]    << "\n";
+        ss << "UpArm:      " << values[UPARM_ROLL]      << "    " << values[UPARM_PITCH]    << "    " << values[UPARM_YAW]      << "\n";
+
+        return ss.str();
+    }
+
+
 };
