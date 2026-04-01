8263d98e717fd05fbdcebf4b727805a083a1db21
ethanbowering24 <ethanbowering24@gmail.com>
2026-04-01 15:15:46 -0400

    move all motors attempt

--- PiClaw_Server/src/robot.cpp
+++ PiClaw_Server/src/robot.cpp
@@ -1,5 +1,6 @@
 #include "robot.h"
 #include "pigpio.h"
+#include "packet.h"
 #include <iostream>
 
 bool Robot::Initialize()
@@ -26,5 +27,11 @@ bool Robot::Initialize()
 
 bool Robot::Move(Packet& packet) //TODO this needs to now write angles to the servos
 {
+    servos[SERVO_WRIST_ROLL].writeAngle(packet.values[WRIST_ROLL]);
+    servos[SERVO_WRIST_PITCH].writeAngle(packet.values[WRIST_PITCH]-packet.values[FOREARM_PITCH]-packet.values[UPARM_PITCH]);
+    servos[SERVO_FOREARM_PITCH].writeAngle(packet.values[FOREARM_PITCH]-packet.values[UPARM_PITCH]);
+    servos[SERVO_UPARM_PITCH].writeAngle(packet.values[UPARM_PITCH]);
+    servos[SERVO_UPARM_YAW].writeAngle(packet.values[FOREARM_YAW]);
+    servos[SERVO_CLAW].writeAngle(packet.values[CLAW]);
     return true;
 }
