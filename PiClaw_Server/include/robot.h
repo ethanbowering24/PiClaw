6bcc753653a249d255ba1e7fbdbfee4f69bd7b5e
ethanbowering24 <ethanbowering24@gmail.com>
2026-04-06 11:12:17 -0400

    change pinout

--- PiClaw_Server/include/robot.h
+++ PiClaw_Server/include/robot.h
@@ -3,12 +3,12 @@
 #include "servo.h"
 #include <array>
 
-constexpr int WRISTROLL_PIN     = 2;
-constexpr int WRISTPITCH_PIN    = 3;
-constexpr int FOREARMPITCH_PIN  = 4;
-constexpr int UPARMPITCH_PIN    = 5;
-constexpr int UPARMYAW_PIN      = 6;
-constexpr int CLAW_PIN          = 7;
+constexpr int CLAW_PIN          = 2;
+constexpr int WRISTROLL_PIN     = 3;
+constexpr int WRISTPITCH_PIN    = 4;
+constexpr int FOREARMPITCH_PIN  = 5;
+constexpr int UPARMPITCH_PIN    = 6;
+constexpr int UPARMYAW_PIN      = 7;
 
 
 class Robot
@@ -27,12 +27,12 @@ class Robot
         };
 
         std::array<Servo, SERVO_COUNT> servos {{
+            {CLAW_PIN,         std::pair<int,int>{0, 180},  0,  0, "Claw"},
             {WRISTROLL_PIN,    std::pair<int,int>{0, 180},  0,  0, "WristRoll"},
             {WRISTPITCH_PIN,   std::pair<int,int>{0, 180}, 90,  90, "WristPitch"},
             {FOREARMPITCH_PIN, std::pair<int,int>{0, 180},  0,	0, "ForearmPitch"},
             {UPARMPITCH_PIN,   std::pair<int,int>{0, 180}, 90,  0, "UpArmPitch"},
             {UPARMYAW_PIN,     std::pair<int,int>{0, 180}, 90,  0, "UpArmYaw"},
-            {CLAW_PIN,         std::pair<int,int>{0, 180},  0,  0, "Claw"},
         }};
 
     public:
